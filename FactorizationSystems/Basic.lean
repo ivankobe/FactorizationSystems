24585eec1fab7b4e034743eb55bd7bc875242df2
Ivan Kobe <ivan.kobe@gmail.com>
2024-11-20 21:53:04 +0100

    added docstrings

--- FactorizationSystems/Basic.lean
+++ FactorizationSystems/Basic.lean
@@ -5,44 +5,64 @@ import Mathlib.CategoryTheory.Limits.Shapes.Terminal
 import Mathlib.CategoryTheory.ClosedUnderIsomorphisms
 import Mathlib.CategoryTheory.MorphismProperty.Composition
 
--- set_option diagnostics true
-
 namespace CategoryTheory
 universe u v u' v'
 variable {C : Type u} [Category.{v} C] {D : Type u'} [Category.{v'} D]
 
+/-- The predicate that a class of morphism contains the isomorphisms -/
 def contains_isos (W : MorphismProperty C) : Prop :=
   ∀ ⦃X Y : C⦄ (f : X ≅ Y) , W f.hom
 
+/-- The predicate of a class of morphisms being closed under compositin -/
 class is_closed_comp (W : MorphismProperty C) extends W.Respects W where
 
+/-- The structure of a factorization system on a category C with specified classes of morphisms -/
 structure FactorizationSystem (L R : MorphismProperty C) where
+  /-- The left class contains isomorphism -/
   contains_isos_left_class : contains_isos L
+  /-- The right class contains isomorphism -/
   contains_isos_right_class : contains_isos R
+  /-- The left class is closed under composition -/
   is_closed_comp_left_class : is_closed_comp L
+  /-- The right class is closed under composition -/
   is_closed_comp_right_class : is_closed_comp R
+  /-- The midpoint/image of the factorization-/
   image  : {X Y : C} → (f : X ⟶ Y) → C
+  /-- The left map of the factorization -/
   left_map : {X Y : C} → (f : X ⟶ Y) → X ⟶ image f
+  /-- The left map of the factorization is contained in the left class-/
+  left_map_in_left_class : {X Y : C} → (f : X ⟶ Y) → L (left_map f)
+  /-- The right map of the factorization -/
   right_map : {X Y : C} → (f : X ⟶ Y) → image f ⟶ Y
+  /-- The left map of the factorization is contained in the left class-/
+  right_map_in_right_class : {X Y : C} → (f : X ⟶ Y) → R (left_map f)
+  /-- The factorization-/
   factorization : {X Y : C} → (f : X ⟶ Y) → left_map f ≫ right_map f = f := by aesop_cat
+  /-- The factorization is unique up to isomorphism -/
   iso :
     {X Y : C} → (f : X ⟶ Y) → (im : C) → (left : X ⟶ im) → (right : im ⟶ Y) →
     (fact : left ≫ right = f) →
     Σ' i : image f ≅ im, left_map f ≫ i.hom = left ∧ i.hom ≫ right = right_map f
+  /-- The factorization is unique up toa unique isomorphism -/
   is_unique_iso :
     {X Y : C} → (f : X ⟶ Y) → (im : C) → (left : X ⟶ im) → (right : im ⟶ Y) →
     (fact : left ≫ right = f) → (i : image f ≅ im) → (comm₁ : left_map f ≫ i.hom = left) →
     (comm₂ : i.hom ≫ right = right_map f) → i = (iso f im left right fact).fst
 
+/-- A class of morphisms in C defines a class of morphism in the slice C/X for every X ∈ C -/
 def MorphismPropertySlice (W : MorphismProperty C) (X : C) : MorphismProperty (Over X) := by
   rintro _ _ f
   exact W ((Over.forget X).map f)
 
+/-- If a class of morphisms contains isomorphisms,
+then so does the class of morphisms in the slice -/
 lemma contains_isos_slice : {W : MorphismProperty C} → {X : C} →  contains_isos W →
     contains_isos (MorphismPropertySlice W X) := by
   intro _ X h _ _ i
   exact h (asIso ((Over.forget X).map i.hom))
 
+/-- If a class of morphisms is closed under composition,
+then so does the class of morphisms in the slice -/
 lemma is_closed_comp_slice {W : MorphismProperty C} {X : C} (h : is_closed_comp W) :
     is_closed_comp (MorphismPropertySlice W X) where
   precomp f hf g hg := by
@@ -56,7 +76,8 @@ lemma is_closed_comp_slice {W : MorphismProperty C} {X : C} (h : is_closed_comp 
 
 namespace Over
 
-lemma forget_map_comm :
+/-- If a triangle commutes in the slice C/X, then it commutes in C -/
+lemma forget_map_comp :
     {X : C} → {p q r : Over X} → (F : p ⟶ q) → (G : q ⟶ r) → (H : p ⟶ r) →
     (hyp : F ≫ G = H) → (F.left ≫ G.left = H.left) := by
   rintro X _ _ _ ⟨f,_,u⟩ ⟨g,_,v⟩ ⟨h,_,w⟩ _
@@ -67,18 +88,20 @@ lemma forget_map_comm :
     _ = ((Over.forget X).map ⟨h,_,w⟩) := by aesop_cat
     _ = h := by simp
 
+/-- The forgetful functor C/X ⟶ X preserves isomorphisms -/
 def forget_preserves_isos : {X : C} → {f g : Over X} → (i : f ≅ g) → f.left ≅ g.left := by
   rintro X _ _ i
   exact
   {
     hom := by exact i.hom.left,
     inv := by exact i.inv.left,
-    hom_inv_id := by exact forget_map_comm _ _ _ i.hom_inv_id,
-    inv_hom_id := by exact forget_map_comm _ _ _ i.inv_hom_id
+    hom_inv_id := by exact forget_map_comp _ _ _ i.hom_inv_id,
+    inv_hom_id := by exact forget_map_comp _ _ _ i.inv_hom_id
   }
 
 end Over
 
+/-- A factorization system in C determines descends to a factorization system in the slice -/
 def FactorizationSystemSlice : {X : C} → {L R : MorphismProperty C} → FactorizationSystem L R →
     FactorizationSystem (MorphismPropertySlice L X) (MorphismPropertySlice R X) := by
   intro X L R F
@@ -100,16 +123,24 @@ def FactorizationSystemSlice : {X : C} → {L R : MorphismProperty C} → Factor
           _ = f ≫ b := by exact (F.factorization f) =≫ b
           _ = a := by simp at w ; exact w
       exact Over.homMk (F.left_map f) comm,
+    left_map_in_left_class := by
+      rintro _ _ ⟨f,_,_⟩
+      have el := F.left_map_in_left_class f
+      aesop_cat
     right_map := by
       rintro _ _ ⟨f,_,_⟩
       exact Over.homMk (F.right_map f) (by aesop_cat),
+    right_map_in_right_class := by
+      rintro _ _ ⟨f,_,_⟩
+      have el := F.right_map_in_right_class f
+      aesop_cat
     factorization := by
       rintro _ _ ⟨f,_,_⟩
       have fact : (F.left_map f) ≫ (F.right_map f) = f := F.factorization f
       aesop_cat,
     iso := by
       rintro ⟨_,_,a⟩ ⟨_,_,b⟩ ⟨g,_,u⟩ ⟨_,_,im⟩ ⟨l,_,v⟩ ⟨r,_,w⟩ fact
-      have ⟨i,⟨P,Q⟩⟩ := F.iso g _ l r (Over.forget_map_comm _ _ _ fact)
+      have ⟨i,⟨P,Q⟩⟩ := F.iso g _ l r (Over.forget_map_comp _ _ _ fact)
       exact
       {
         fst := by
@@ -125,9 +156,9 @@ def FactorizationSystemSlice : {X : C} → {L R : MorphismProperty C} → Factor
       rintro ⟨A,_,a⟩ ⟨B,_,b⟩ ⟨g,_,u⟩ ⟨Im,_,im⟩ ⟨l,_,v⟩ ⟨r,_,w⟩
       rintro fact i comm₁ comm₂
       have duh := F.is_unique_iso g Im l r
-        (Over.forget_map_comm _ _ _ fact)
+        (Over.forget_map_comp _ _ _ fact)
         (Over.forget_preserves_isos i)
-        (Over.forget_map_comm _ _ _ comm₁)
-        (Over.forget_map_comm _ _ _ comm₂)
+        (Over.forget_map_comp _ _ _ comm₁)
+        (Over.forget_map_comp _ _ _ comm₂)
       aesop_cat
   }
