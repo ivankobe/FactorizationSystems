a1cb8d169f14e428e82ca525ad2f136b138cc24c
Ivan Kobe <ivan.kobe@gmail.com>
2025-01-19 14:13:34 +0100

    wip blueprint

--- FactorizationSystems.lean
+++ FactorizationSystems.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `FactorizationSystems` library.
 -- Import modules here that should be built as part of the library.
 import FactorizationSystems.Basic
+import FactorizationSystems.Examples
+import FactorizationSystems.Orthogonality
+import FactorizationSystems.OrthogonalComplements
+import FactorizationSystems.Characterization
